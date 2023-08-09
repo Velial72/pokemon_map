67bf72e44e59166af9d2f18f9e3112a12334d5c3
Velial72 <abramov1991a@gmail.com>
2023-08-09 23:38:54 +0500

    add null\blank

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,9 +3,9 @@ from django.db import models  # noqa F401
 # your models here
 class Pokemon(models.Model):
     title = models.TextField(verbose_name='Название')
-    title_en = models.TextField(verbose_name='Название на английском')
-    title_jp = models.TextField(verbose_name='Название на японском')
-    description = models.TextField(verbose_name='Описание')
+    title_en = models.TextField(verbose_name='Название на английском', blank=True)
+    title_jp = models.TextField(verbose_name='Название на японском', blank=True)
+    description = models.TextField(verbose_name='Описание', blank=True)
     next_evolution = models.ForeignKey("self",
                                        verbose_name='В кого эволюционирует',
                                        null=True,
@@ -19,11 +19,11 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, verbose_name='Покемон', related_name='entities', on_delete=models.CASCADE)
-    lat = models.FloatField(verbose_name='Широта', blank=True)
-    lon = models.FloatField(verbose_name='Долгота', blank=True)
-    appeared_at = models.DateTimeField(verbose_name='Появляется', blank=True)
-    disappeared_at = models.DateTimeField(verbose_name='Пропадает', blank=True)
-    level = models.IntegerField(verbose_name='Уровень', blank=True)
+    lat = models.FloatField(verbose_name='Широта', null=True)
+    lon = models.FloatField(verbose_name='Долгота', null=True)
+    appeared_at = models.DateTimeField(verbose_name='Появляется', null=True)
+    disappeared_at = models.DateTimeField(verbose_name='Пропадает', null=True)
+    level = models.IntegerField(verbose_name='Уровень', null=True)
     health = models.IntegerField(verbose_name='Здоровье', blank=True)
     attack = models.IntegerField(verbose_name='Атака', blank=True)
     defence = models.IntegerField(verbose_name='Защита', blank=True)
